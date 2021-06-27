78f2d49ae1a689a952818ce804792504320d46a0
Sumit Saini <sumitsaini199711@gmail.com>
2021-06-27 23:04:32 +0530

    Setting up Mikro-orm and connecting with local postgresql db.

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello from Js file");
+import {MikroORM} from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+
+const main = async ()=>{
+    let orm = await MikroORM.init({
+        entities: [Post],
+        dbName:'lireddit',
+        type:'postgresql',
+        debug: !__prod__,
+    });
+}
+
+main();
